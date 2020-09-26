93e3472774599bed5184a2aa6a06051b9c033778
RedstoneParadox <spinjitzu00@gmail.com>
2020-09-26 11:40:04 -0700

    Initial commit

---
+++ Scripts/Export/BrushFace.cs
@@ -0,0 +1,20 @@
+﻿using Godot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuzzlemakerPro.Scripts.Export
+{
+    struct BrushFace
+    {
+        public Vector3 start;
+        public Vector3 size;
+        public Vector2 uv;
+        public Vector2 uvScale;
+        public string vMaterial;
+        public double rotation;
+        public int lightMapScale;
+    }
+}
