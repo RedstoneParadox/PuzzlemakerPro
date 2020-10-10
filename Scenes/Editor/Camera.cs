396dbe831c937782b5783a232f209bf9f1c3e02a
RedstoneParadox <spinjitzu00@gmail.com>
2020-10-09 21:36:14 -0700

    More camera improvements

--- Scenes/Editor/Camera.cs
+++ Scenes/Editor/Camera.cs
@@ -3,11 +3,7 @@ using System;
 
 public class Camera : Godot.Camera
 {
-    private Vector3 pitchAxis = Vector3.Right;
-    private Vector3 yawAxis = Vector3.Up;
     private PackedScene debug = GD.Load<PackedScene>("res://Scenes/Editor/DEBUG.tscn");
-
-    private float pitch = 0.0f;
     private float length = 20.0f;
 
     public override void _Ready()
@@ -18,17 +14,10 @@ public class Camera : Godot.Camera
     // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(float delta)
     {
-        if (Input.IsActionPressed("ui_up")) pitch -= Mathf.Pi / 90;
-        if (Input.IsActionPressed("ui_down")) pitch += Mathf.Pi / 90;
-
-        pitch = Mathf.Clamp(pitch, -Mathf.Pi / 2 + 0.00001f, Mathf.Pi / 2 - 0.00001f);
 
         if (length < 0.01f) length = 0.01f;
 
-        var translation = new Vector3(0, 0, length);
-        translation = translation.Rotated(new Vector3(1, 0, 0), pitch);
-
-        Translation = translation;
+        Translation = new Vector3(0, 0, length);
         LookAt(GetParent<Spatial>().Translation, Vector3.Up);
 
         if (Input.IsActionJustPressed("debug_place"))
@@ -54,11 +43,11 @@ public class Camera : Godot.Camera
             {
                 if (emb.ButtonIndex == (int)ButtonList.WheelUp)
                 {
-                    length -= 0.1f;
+                    length -= 0.25f;
                 }
                 if (emb.ButtonIndex == (int)ButtonList.WheelDown)
                 {
-                    length += 0.1f;
+                    length += 0.25f;
                 }
             }
         }
--- Scenes/Editor/CameraBase.cs
+++ Scenes/Editor/CameraBase.cs
@@ -4,6 +4,7 @@ using System;
 public class CameraBase : Spatial
 {
     private float yaw = 0.0f;
+    private float pitch = 0.0f;
     private float panSpeed = 0.25f;
 
     public override void _Ready()
@@ -25,7 +26,13 @@ public class CameraBase : Spatial
         if (Input.IsActionPressed("ui_left")) yaw -= Mathf.Pi / 90;
         if (Input.IsActionPressed("ui_right")) yaw += Mathf.Pi / 90;
 
-        Rotation = new Vector3(0, yaw, 0);
-        Translation += targetOffset.Normalized().Rotated(Vector3.Up, yaw) * panSpeed;
+        if (Input.IsActionPressed("ui_up")) pitch -= Mathf.Pi / 90;
+        if (Input.IsActionPressed("ui_down")) pitch += Mathf.Pi / 90;
+
+        pitch = Mathf.Clamp(pitch, -Mathf.Pi / 2 + 0.00001f, Mathf.Pi / 2 - 0.00001f);
+
+        Rotation = new Vector3(pitch, yaw, 0);
+        var pitchAxis = Vector3.Right.Rotated(Vector3.Up, yaw);
+        Translation += targetOffset.Normalized().Rotated(Vector3.Up, yaw).Rotated(pitchAxis, pitch) * panSpeed;
     }
 }
