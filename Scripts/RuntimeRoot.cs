5288e151c9053f7c1ec55581bd974215c9c2a8b1
RedstoneParadox <spinjitzu00@gmail.com>
2020-10-09 21:16:57 -0700

    Made the runtime root scene the actual root of the application

--- Scripts/RuntimeRoot.cs
+++ Scripts/RuntimeRoot.cs
@@ -3,19 +3,10 @@ using System;
 
 public class RuntimeRoot : Node
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
-
-    // Called when the node enters the scene tree for the first time.
+    private readonly PackedScene LevelScene = GD.Load<PackedScene>("res://Scenes/Editor/Level.tscn");
+    
     public override void _Ready()
     {
-        
+        AddChild(LevelScene.Instance());
     }
-
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
 }
