57251d6e4cbd713fe99275271f84a91532a30287
RedstoneParadox <spinjitzu00@gmail.com>
2020-10-12 10:35:53 -0700

    Begin working of framework for multiple textures

---
+++ Scripts/Editor/VoxelFace.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Godot;
+using PuzzlemakerPro.Scripts.Texture;
+
+namespace PuzzlemakerPro.Scripts.Editor
+{
+    class VoxelFace
+    {
+        public VoxelTextureInfo Info;
+        public Vector2 Offset;
+
+        public VoxelFace(VoxelTextureInfo info, Vector2 offset)
+        {
+            Info = info;
+            Offset = offset;
+        }
+    }
+}
---
+++ Scripts/Texture/VoxelTextureInfo.cs
@@ -0,0 +1,24 @@
+﻿using Godot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuzzlemakerPro.Scripts.Texture
+{
+    class VoxelTextureInfo
+    {
+        public readonly Godot.Texture VoxelTexture;
+        public readonly Vector2 Origin;
+        // 1x1 fits on a single voxel face.
+        public readonly Vector2 Size;
+
+        VoxelTextureInfo(Godot.Texture voxelTexture, Vector2 origin, Vector2 size)
+        {
+            VoxelTexture = voxelTexture;
+            Origin = origin;
+            Size = size;
+        }
+    }
+}
