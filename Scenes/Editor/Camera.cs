863e01997dea3f545f4315d2a70e9dbd402410ea
RedstoneParadox <spinjitzu00@gmail.com>
2020-11-14 12:52:38 -0800

    Attempted to fix camera scrolling

--- Scenes/Editor/Camera.cs
+++ Scenes/Editor/Camera.cs
@@ -3,7 +3,6 @@ using System;
 
 public class Camera : Godot.Camera
 {
-    private PackedScene debug = GD.Load<PackedScene>("res://Scenes/Editor/DEBUG.tscn");
     private float length = 20.0f;
 
     public override void _Ready()
@@ -14,29 +13,18 @@ public class Camera : Godot.Camera
     // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(float delta)
     {
+        if (Input.IsActionJustPressed("scroll_up"))
+        {
+            length -= 0.25f;
+        }
+        if (Input.IsActionJustPressed("scroll_down"))
+        {
+            length += 0.25f;
+        }
 
         if (length < 0.01f) length = 0.01f;
 
         Translation = new Vector3(0, 0, length);
         LookAt(GetParent<Spatial>().Translation, Vector3.Up);
-    }
-
-    public override void _UnhandledInput(InputEvent @event)
-    {
-        if (@event is InputEventMouseButton)
-        {
-            InputEventMouseButton emb = (InputEventMouseButton)@event;
-            if (emb.IsPressed())
-            {
-                if (emb.ButtonIndex == (int)ButtonList.WheelUp)
-                {
-                    length -= 0.25f;
-                }
-                if (emb.ButtonIndex == (int)ButtonList.WheelDown)
-                {
-                    length += 0.25f;
-                }
-            }
-        }
     }
 }
