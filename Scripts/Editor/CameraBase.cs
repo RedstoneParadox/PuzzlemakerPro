f9ae2369c27f3596210d7263f9b0ba77a991189b
RedstoneParadox <spinjitzu00@gmail.com>
2020-11-16 21:09:23 -0800

    Move all camera logic into single script and make camera scene

--- Scripts/Editor/CameraBase.cs
+++ Scripts/Editor/CameraBase.cs
@@ -5,6 +5,7 @@ public class CameraBase : Spatial
 {
     private float yaw = Mathf.Pi/4.0f;
     private float pitch = -Mathf.Pi/6.0f;
+    private float length = 20.0f;
     private float panSpeed = 0.25f;
 
     public override void _Ready()
@@ -34,5 +35,20 @@ public class CameraBase : Spatial
         Rotation = new Vector3(pitch, yaw, 0);
         var pitchAxis = Vector3.Right.Rotated(Vector3.Up, yaw);
         Translation += targetOffset.Normalized().Rotated(Vector3.Up, yaw).Rotated(pitchAxis, pitch) * panSpeed;
+
+        if (Input.IsActionJustPressed("scroll_up"))
+        {
+            length -= 0.25f;
+        }
+        if (Input.IsActionJustPressed("scroll_down"))
+        {
+            length += 0.25f;
+        }
+
+        if (length < 0.01f) length = 0.01f;
+
+        Camera camera = GetNode<Camera>("Camera");
+
+        camera.Translation = new Vector3(0, 0, length);
     }
 }
--- Scripts/RuntimeRoot.cs
+++ Scripts/RuntimeRoot.cs
@@ -3,12 +3,12 @@ using System;
 
 public class RuntimeRoot : Node
 {
-    public static Camera CurrentCamera = new Camera();
+    public static Camera CurrentCamera = null;
 
     public override void _Process(float delta)
     {
         base._Process(delta);
         DebugOverlay.Information["FPS"] = Engine.GetFramesPerSecond();
-        DebugOverlay.Information["Camera Position"] = CurrentCamera.GlobalTransform.origin;
+        if (CurrentCamera != null) DebugOverlay.Information["Camera Position"] = CurrentCamera.GlobalTransform.origin;
     }
 }
